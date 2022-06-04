59e70c65d09e2de9792d8b5ee322dc4ef30f1993
Rodrigo Grzegorczyk <rodrigo.gregorio1@hotmail.com>
2022-06-04 13:54:46 -0300

    Autenticação com Github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,27 @@
 import styles from './styles.module.scss';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import { FaGithub } from 'react-icons/fa';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession();
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button
             className={styles.signInButton}
             type='button'
+            onClick={() => signOut()}
         >
             <FaGithub color="#04D361" />
-            Rodrigo Grzegorczyk
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
         <button
-            className={styles.signInButton}
             type='button'
+            className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#EBA417" />
             Sign in with Github
